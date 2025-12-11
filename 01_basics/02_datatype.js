3c5f5d5246aabbf3d82f59f750d98cbb9a97b7db
hemant-7k <155747419+hemant-7k@users.noreply.github.com>
2025-12-11 06:52:28 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+ "use strict"; //treat all JS code as newer version
+
+
+ let name = "Hemant" 
+ let age = 18 
+ let islinkedin = false
+ let state;
+
+//  number => 2 to power 53 
+// bigint => this datatype is use for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value.  (empty value) 
+// undefined => 
+// symbol => unique
+
+
+console.log( typeof undefined); //undefined
+
+console.log( typeof null); //object (bug in js)
