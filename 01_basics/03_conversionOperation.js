20d5e78e76842a261d71a64077b46e8b2153d3e1
hemant-7k <155747419+hemant-7k@users.noreply.github.com>
2025-12-24 18:12:01 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "false";
 
-console.log( typeof score ); // number
+// console.log( typeof score ); // number
 
 // conversion operation
 
 let valueInNumber = Number(score);
 
-console.log( typeof valueInNumber ); // number
-console.log( valueInNumber ); // NaN
+// console.log( typeof valueInNumber ); // number
+// console.log( valueInNumber ); // NaN
 
 // NaN - not a number
 // NaN is a special number which shows that the value is not a valid number
@@ -31,3 +31,36 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+
+// ********************operations in JavaScript**********************
+
+let value = 3;
+let negvalue = -value;
+
+// console.log( negvalue ); // -3
+
+// console.log( 2*2 ); // 4
+// console.log( 3/2 ); // 1.5
+// console.log( 5%2 ); // 1
+// console.log( 2+3 ); // 5
+// console.log( 2-3 ); // -1
+
+let str1 = "hemant"
+let str2 = "hitesh"
+let str3 = str1 + str2
+// console.log( str3 ); // hemanthitesh
+
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( 1 + 2 + "3");
+
+
+let gamecounter = 100
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter);
+
+// link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
